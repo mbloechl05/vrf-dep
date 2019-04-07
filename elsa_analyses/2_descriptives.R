0e82c2473905a96fa1a32d815fec340caaaeb98d
Maria Bloechl <bloechl@cbs.mpg.de>
2019-04-07 12:28:00 +0200

    re-work script descriptives

--- elsa_analyses/2_descriptives.R
+++ elsa_analyses/2_descriptives.R
@@ -3,7 +3,6 @@
 # (contact maria.bloechl@gmail.com in case of questions)
 # ==============================================================
 
-
 # clear work space
 rm(list = ls()) 
 
@@ -11,30 +10,28 @@ rm(list = ls())
 library(psych)
 library(Hmisc)
 library(dplyr)
-library(VennDiagram)
-library(ggplot2)
 
 # load preprocessed data 
 load("data/processed/elsa_proc_data.RData")
 
 
-# ------------------------------------------
-# 1) Baseline descriptives sample (wave 2)
-# ------------------------------------------
+# ---------------------------
+# 1) Baseline descriptives
+# ---------------------------
 
-# cont. vars (age, blood pressure, BMI, cholesterol, hdl)
+# descriptive stats for continuous variables (#binary vars only included to get valid Ns)
 psych::describe(data[,c("w2_dhager", "w2_bmival", "w2_meansys", "w2_chol_f", "w2_hdl_f", "w2_ldl_f",  
-                        "w2_DhSex", "w2_fqethnr", "w0_educ", "hyp", "diab", "w2_HESka", "n_rf")]) #binary vars only included to get valid n
+                        "w2_DhSex", "w2_fqethnr", "w0_educ", "hyp", "diab", "w2_HESka", "n_rf")]) 
 
-# binary vars (gender, ethnicity, diabetes, current smoking) 
+# descriptive stats for binary vars  
 tables <- lapply(data[,c("w2_DhSex", "w2_fqethnr", "w0_educ", "hyp", "diab", "w2_HESka", "n_rf")], table)
-tables
-lapply(tables, prop.table)
+tables # absolute frequencies
+lapply(tables, prop.table) # relative frequencies
 
 
-# --------------------------
-# 2) Correlation matrices
-# --------------------------
+# ------------------------------------------------
+# 2) Supplement: Zero-order correlation matrices
+# ------------------------------------------------
 
 # select variables for corr matrix
 vars <- data[,c("w2_dhager","w2_DhSex", "w2_fqethnr", "w0_educ", "hyp",
@@ -43,31 +40,4 @@ vars <- data[,c("w2_dhager","w2_DhSex", "w2_fqethnr", "w0_educ", "hyp",
 # create corr matrix (rounded to 2 dec places)
 rcorr(as.matrix(vars))
 
-
-# -------------------------------------------------
-# 3) Co-occurences vascular risk factors (wave 1)
-# -------------------------------------------------
-
-# create seperte data frame with risk factor variables
-rf  <- data[,c("hyp","diab","w2_HESka", "w2_bmi.b")] # BMI as binary
-
-## this is a custom function to plot a venn diagram for the vascular risk factors
-# create function that allows to analyse co-morbidities
-cm <- function(riskfact) {
-  names(rf) <- c(1, 2, 3, 4) # 1 = diab, 2 = hyp, 3 = smok, 4 = obes
-  for (i in 1:length(riskfact)) {
-    rf <- subset(rf, rf[riskfact[i]] == 1)
-  }
-  nrow(rf)
-}
-
-# Venn diagramm co-occurence of risk factors
-grid.newpage()
-draw.quad.venn(
-  cm(1), cm(2), cm(3), cm(4), 
-  cm(c(1,2)), cm(c(1,3)), cm(c(1,4)), cm(c(2,3)), cm(c(2,4)), cm(c(3,4)), 
-  cm(c(1,2,3)), cm(c(1,2,4)), cm(c(1,3,4)), cm(c(2,3,4)), cm(c(1,2,3,4)), 
-  category = c("Hypertension", "Diabetes", "Current smoking", "BMI > 30"), 
-  lty = 3, col = "white", fill = c("#a2d1ec", "#a8bef7", "#aab0ef", "#d7c7f9"), 
-  fontfamily = "sans", cat.fontfamily = "sans", alpha=c(0.4, 0.4,0.4,0.4))
 
