7f0e27faa336968a7a66bac0e2aa753ef2babd60
Maria Bloechl <bloechl@cbs.mpg.de>
2019-04-16 11:05:06 +0200

    corrections MIDUS measurement invariance and growth models

--- midus_analyses/4_growthmodels.R
+++ midus_analyses/4_growthmodels.R
@@ -7,16 +7,9 @@
 rm(list = ls())
 
 # load packages 
-library(lme4)
-library(lmerTest)
 library(lavaan)
 library(semTools)
-library(psych)
 library(ggplot2)
-library(dplyr)
-library(plyr)
-library(DescTools)
-library(cobalt)
 
 # load preprocessed data 
 load("data/midus/processed/midus_proc_data.RData")
@@ -35,9 +28,9 @@ W2_DEP =~ 1*B1SA24A + l1*B1SA24B + l2*B1SA24D + l3*B1SA24E + l4*B1SA24F
 W3_DEP =~ 1*C1SA20A + l1*C1SA20B + l2*C1SA20D + l3*C1SA20E + l4*C1SA20F
 
 # indicator intercepts constrained equal
-A1SA13A ~ ai*1
-B1SA24A ~ ai*1
-C1SA20A ~ ai*1
+A1SA13A ~ 0*1
+B1SA24A ~ 0*1
+C1SA20A ~ 0*1
 
 A1SA13B ~ bi*1
 B1SA24B ~ bi*1
@@ -51,9 +44,9 @@ A1SA13E ~ ei*1
 B1SA24E ~ ei*1
 C1SA20E ~ ei*1
 
-A1SA13F ~ di*1
-B1SA24F ~ di*1
-C1SA20F ~ di*1
+A1SA13F ~ fi*1
+B1SA24F ~ fi*1
+C1SA20F ~ fi*1
 
 # define latent factor means
 W1_DEP ~ 0*1
@@ -61,16 +54,16 @@ W2_DEP ~ 0*1
 W3_DEP ~ 0*1
 
 # residual correlations allowed
-A1SA13A ~~ ac*B1SA24A + ac*C1SA20A
-B1SA24A ~~ ac*C1SA20A
-A1SA13B ~~ bc*B1SA24B + bc*C1SA20B
-B1SA24B ~~ bc*C1SA20B
-A1SA13D ~~ dc*B1SA24D + dc*C1SA20D
-B1SA24D ~~ dc*C1SA20D
-A1SA13E ~~ ec*B1SA24E + ec*C1SA20E
-B1SA24E ~~ ec*C1SA20E
-A1SA13F ~~ fc*B1SA24F + fc*C1SA20F
-B1SA24F ~~ fc*C1SA20F
+A1SA13A ~~ B1SA24A + C1SA20A
+B1SA24A ~~ C1SA20A
+A1SA13B ~~ B1SA24B + C1SA20B
+B1SA24B ~~ C1SA20B
+A1SA13D ~~ B1SA24D + C1SA20D
+B1SA24D ~~ C1SA20D
+A1SA13E ~~ B1SA24E + C1SA20E
+B1SA24E ~~ C1SA20E
+A1SA13F ~~ B1SA24F + C1SA20F
+B1SA24F ~~ C1SA20F
 
 # residual variances constrained
 A1SA13A ~~ ae*A1SA13A
@@ -103,7 +96,7 @@ s ~~ s
 i ~~ s
 
 # intercept and slope means
-i ~ 0*1
+i ~ 1
 s ~ 1
 '
 
@@ -127,9 +120,9 @@ W2_DEP =~ 1*B1SA24A + l1*B1SA24B + l2*B1SA24D + l3*B1SA24E + l4*B1SA24F
 W3_DEP =~ 1*C1SA20A + l1*C1SA20B + l2*C1SA20D + l3*C1SA20E + l4*C1SA20F
 
 # indicator intercepts constrained equal
-A1SA13A ~ ai*1
-B1SA24A ~ ai*1
-C1SA20A ~ ai*1
+A1SA13A ~ 0*1
+B1SA24A ~ 0*1
+C1SA20A ~ 0*1
 
 A1SA13B ~ bi*1
 B1SA24B ~ bi*1
@@ -143,9 +136,9 @@ A1SA13E ~ ei*1
 B1SA24E ~ ei*1
 C1SA20E ~ ei*1
 
-A1SA13F ~ di*1
-B1SA24F ~ di*1
-C1SA20F ~ di*1
+A1SA13F ~ fi*1
+B1SA24F ~ fi*1
+C1SA20F ~ fi*1
 
 # define latent factor means
 W1_DEP ~ 0*1
@@ -153,16 +146,16 @@ W2_DEP ~ 0*1
 W3_DEP ~ 0*1
 
 # residual correlations allowed
-A1SA13A ~~ ac*B1SA24A + ac*C1SA20A
-B1SA24A ~~ ac*C1SA20A
-A1SA13B ~~ bc*B1SA24B + bc*C1SA20B
-B1SA24B ~~ bc*C1SA20B
-A1SA13D ~~ dc*B1SA24D + dc*C1SA20D
-B1SA24D ~~ dc*C1SA20D
-A1SA13E ~~ ec*B1SA24E + ec*C1SA20E
-B1SA24E ~~ ec*C1SA20E
-A1SA13F ~~ fc*B1SA24F + fc*C1SA20F
-B1SA24F ~~ fc*C1SA20F
+A1SA13A ~~ B1SA24A + C1SA20A
+B1SA24A ~~ C1SA20A
+A1SA13B ~~ B1SA24B + C1SA20B
+B1SA24B ~~ C1SA20B
+A1SA13D ~~ B1SA24D + C1SA20D
+B1SA24D ~~ C1SA20D
+A1SA13E ~~ B1SA24E + C1SA20E
+B1SA24E ~~ C1SA20E
+A1SA13F ~~ B1SA24F + C1SA20F
+B1SA24F ~~ C1SA20F
 
 # residual variances constrained
 A1SA13A ~~ ae*A1SA13A
@@ -195,7 +188,7 @@ s ~~ s
 i ~~ s
 
 # intercept and slope means
-i ~ 0*1
+i ~ 1
 s ~ 1
 
 # regressions
@@ -226,10 +219,15 @@ W1_DEP =~ 1*A1SA13A + l1*A1SA13B + l2*A1SA13D + l3*A1SA13E + l4*A1SA13F
 W2_DEP =~ 1*B1SA24A + l1*B1SA24B + l2*B1SA24D + l3*B1SA24E + l4*B1SA24F
 W3_DEP =~ 1*C1SA20A + l1*C1SA20B + l2*C1SA20D + l3*C1SA20E + l4*C1SA20F
 
+# define latent factor means
+W1_DEP ~ 0*1
+W2_DEP ~ 0*1
+W3_DEP ~ 0*1
+
 # indicator intercepts constrained equal
-A1SA13A ~ ai*1
-B1SA24A ~ ai*1
-C1SA20A ~ ai*1
+A1SA13A ~ 0*1
+B1SA24A ~ 0*1
+C1SA20A ~ 0*1
 
 A1SA13B ~ bi*1
 B1SA24B ~ bi*1
@@ -243,21 +241,21 @@ A1SA13E ~ ei*1
 B1SA24E ~ ei*1
 C1SA20E ~ ei*1
 
-A1SA13F ~ di*1
-B1SA24F ~ di*1
-C1SA20F ~ di*1
+A1SA13F ~ fi*1
+B1SA24F ~ fi*1
+C1SA20F ~ fi*1
 
 # residual correlations allowed
-A1SA13A ~~ ac*B1SA24A + ac*C1SA20A
-B1SA24A ~~ ac*C1SA20A
-A1SA13B ~~ bc*B1SA24B + bc*C1SA20B
-B1SA24B ~~ bc*C1SA20B
-A1SA13D ~~ dc*B1SA24D + dc*C1SA20D
-B1SA24D ~~ dc*C1SA20D
-A1SA13E ~~ ec*B1SA24E + ec*C1SA20E
-B1SA24E ~~ ec*C1SA20E
-A1SA13F ~~ fc*B1SA24F + fc*C1SA20F
-B1SA24F ~~ fc*C1SA20F
+A1SA13A ~~ B1SA24A + C1SA20A
+B1SA24A ~~ C1SA20A
+A1SA13B ~~ B1SA24B + C1SA20B
+B1SA24B ~~ C1SA20B
+A1SA13D ~~ B1SA24D + C1SA20D
+B1SA24D ~~ C1SA20D
+A1SA13E ~~ B1SA24E + C1SA20E
+B1SA24E ~~ C1SA20E
+A1SA13F ~~ B1SA24F + C1SA20F
+B1SA24F ~~ C1SA20F
 
 # residual variances constrained
 A1SA13A ~~ ae*A1SA13A
@@ -284,16 +282,14 @@ C1SA20F ~~ fe*C1SA20F
 i =~ 1*W1_DEP + 1*W2_DEP + 1*W3_DEP
 s =~ 0*W1_DEP + 1*W2_DEP + 2*W3_DEP
 
+# intercept and slope var, covar
 i ~~ i
 s ~~ s
 i ~~ s
 
-i ~ 0*1
+# intercept and slope means
+i ~ 1
 s ~ 1
-
-W1_DEP ~ 0*1
-W2_DEP ~ 0*1
-W3_DEP ~ 0*1
 
 # regressions
 i ~ A1PAGE_M2.c + A1PRSEX.n + A1SS7.n + A1PB1.n + pred
@@ -368,10 +364,15 @@ W1_DEP =~ 1*A1SA13A + l1*A1SA13B + l2*A1SA13D + l3*A1SA13E + l4*A1SA13F
 W2_DEP =~ 1*B1SA24A + l1*B1SA24B + l2*B1SA24D + l3*B1SA24E + l4*B1SA24F
 W3_DEP =~ 1*C1SA20A + l1*C1SA20B + l2*C1SA20D + l3*C1SA20E + l4*C1SA20F
 
+# define latent factor means
+W1_DEP ~ 0*1
+W2_DEP ~ 0*1
+W3_DEP ~ 0*1
+
 # indicator intercepts constrained equal
-A1SA13A ~ ai*1
-B1SA24A ~ ai*1
-C1SA20A ~ ai*1
+A1SA13A ~ 0*1
+B1SA24A ~ 0*1
+C1SA20A ~ 0*1
 
 A1SA13B ~ bi*1
 B1SA24B ~ bi*1
@@ -385,21 +386,21 @@ A1SA13E ~ ei*1
 B1SA24E ~ ei*1
 C1SA20E ~ ei*1
 
-A1SA13F ~ di*1
-B1SA24F ~ di*1
-C1SA20F ~ di*1
+A1SA13F ~ fi*1
+B1SA24F ~ fi*1
+C1SA20F ~ fi*1
 
 # residual correlations allowed
-A1SA13A ~~ ac*B1SA24A + ac*C1SA20A
-B1SA24A ~~ ac*C1SA20A
-A1SA13B ~~ bc*B1SA24B + bc*C1SA20B
-B1SA24B ~~ bc*C1SA20B
-A1SA13D ~~ dc*B1SA24D + dc*C1SA20D
-B1SA24D ~~ dc*C1SA20D
-A1SA13E ~~ ec*B1SA24E + ec*C1SA20E
-B1SA24E ~~ ec*C1SA20E
-A1SA13F ~~ fc*B1SA24F + fc*C1SA20F
-B1SA24F ~~ fc*C1SA20F
+A1SA13A ~~ B1SA24A + C1SA20A
+B1SA24A ~~ C1SA20A
+A1SA13B ~~ B1SA24B + C1SA20B
+B1SA24B ~~ C1SA20B
+A1SA13D ~~ B1SA24D + C1SA20D
+B1SA24D ~~ C1SA20D
+A1SA13E ~~ B1SA24E + C1SA20E
+B1SA24E ~~ C1SA20E
+A1SA13F ~~ B1SA24F + C1SA20F
+B1SA24F ~~ C1SA20F
 
 # residual variances constrained
 A1SA13A ~~ ae*A1SA13A
@@ -426,16 +427,14 @@ C1SA20F ~~ fe*C1SA20F
 i =~ 1*W1_DEP + 1*W2_DEP + 1*W3_DEP
 s =~ 0*W1_DEP + 1*W2_DEP + 2*W3_DEP
 
+# intercept and slope var, covar
 i ~~ i
 s ~~ s
 i ~~ s
 
-i ~ 0*1
+# intercept and slope means
+i ~ 1
 s ~ 1
-
-W1_DEP ~ 0*1
-W2_DEP ~ 0*1
-W3_DEP ~ 0*1
 
 # regressions
 i ~ A1PAGE_M2.c + A1PRSEX.n + A1SS7.n + A1PB1.n + A1PA43.n + A1SA9S.n + A1SBMI + A1SA9X.n
