11a4b8cd2c6503dc56ac338e53fb0f606424f2bc
Maria Bloechl <bloechl@cbs.mpg.de>
2019-04-18 11:46:41 +0200

    variable selection update

--- midus_analyses/1_preproc.R
+++ midus_analyses/1_preproc.R
@@ -21,44 +21,13 @@ load("data/midus/raw/02760-0001-Data.rda") # wave 1
 load("data/midus/raw/04652-0001-Data.rda") # wave 2
 load("data/midus/raw/36346-0001-Data.rda") # wave 3
 
-
 # select only relevant varibales from waves (see codebook for explanations)
-wave1_s <- da02760.0001[,c("M2ID", "A1PAGE_M2", "A1SS7", "A1PRSEX", "A1PB1", 
-                           "A1PA4", "A1PA5", "A1PA11BC", "A1SA9X", "A1PA40", "A1SBMI",
-                           "A1PA36", "A1SA9Y", "A1SA9S", "A1SA10A", "A1SA10C", 
-                           "A1SA13A", "A1SA13B", "A1SA13C", "A1SA13D", "A1SA13E", "A1SA13F", 
-                           "A1SA15A", "A1SA15B", "A1SA15C", "A1SA15D", "A1SA15E", "A1SA15F",
-                           "A1SA9Z", "A1SA10K",
-                           "A1PA31", "A1PA33", 
-                           "A1SF1A", "A1SF1B", "A1SF1C", "A1SF1D", "A1SF1E", "A1SF1F", "A1SF1G", "A1SF1H", "A1SF1I", 
-                           "A1SF1J", "A1SF1K", "A1SF1L", "A1SF1M", "A1SF1N", "A1SF1O", "A1SF1P", "A1SF1Q", "A1SF1R", 
-                           "A1PA43", "A1PA41")]
+wave1_s <- da02760.0001[,c("M2ID", "A1PAGE_M2", "A1PRSEX", "A1SS7",  "A1PB1", "A1SA9X", "A1PA43", "A1SBMI", "A1SA9S", 
+                           "A1SA13A", "A1SA13B", "A1SA13C", "A1SA13D", "A1SA13E", "A1SA13F", "A1PA33", "A1SA10A","A1SA10K")]
 
-wave2_s <- da04652.0001[,c("M2ID", "B1PAGE_M2", "B1PB1", "B1PF7A", "B1PB19", "B1PB30",
-                           "B1PA1", "B1PA2", "B1PA7BC", "B1SA11X", "B1PA38A", "B1SBMI", 
-                           "B1PDEPAF", "B1PANHED", "B1PDEPRE",
-                           "B1SA11S", "B1PA12", "B1SA12A", "B1SA12C",
-                           "B1PA26", "B1PA8", "B1PA6D", "B1SA11Y", 
-                           "B1SA24A", "B1SA24B", "B1SA24C", "B1SA24D", "B1SA24E", "B1SA24F", 
-                           "B1SA26A", "B1SA26B", "B1SA26C", "B1SA26D", "B1SA26E", "B1SA26F",
-                           "B1SA11Z", "B1PA6A", "B1SA12K",
-                           "B1SA28A", "B1SA28B", "B1SA28C", "B1SA28E", "B1SA28F", "B1SA28G", "B1SA28H", "B1SA28I","B1SA28J", 
-                           "B1SE1X", "B1SE1F", "B1SE1OO", "B1SE1H", "B1SE1DD", "B1SE1J", "B1SE1E", "B1SE1B", "B1SE1T", 
-                           "B1SE1QQ", "B1SE1AA", "B1SE1I", "B1SE1BB", "B1SE1GG", "B1SE1M", "B1SE1HH", "B1SE1S", "B1SE1KK", 
-                           "B1PA39", "B1PA37")]
+wave2_s <- da04652.0001[,c("M2ID", "B1PAGE_M2", "B1SA24A", "B1SA24B", "B1SA24C", "B1SA24D", "B1SA24E", "B1SA24F")]
 
-wave3_s <- da36346.0001[,c("M2ID", "C1PRAGE", "C1PB1", 
-                           "C1PA1", "C1PA2", "C1PA7BC", "C1SA11X", "C1PA38A", "C1SBMI", 
-                           "C1PDEPAF", "C1PANHED", "C1PDEPRE",
-                           "C1PA26", "C1PA8","C1SA11Y",
-                           "C1SA20A", "C1SA20B", "C1SA20C", "C1SA20D", "C1SA20E", "C1SA20F", 
-                           "C1SA22A", "C1SA22B", "C1SA22C", "C1SA22D", "C1SA22E", "C1SA22F",
-                           "C1SA11Z", "C1PA6A", 
-                           "C1SA24A", "C1SA24B", "C1SA24C", "C1SA24E", "C1SA24F", "C1SA24G", "C1SA24H", "C1SA24I", "C1SA24J", 
-                           "C1SNEGAF", "C1SPOSAF", "C1SA12K", 
-                           "C1SE1X", "C1SE1F", "C1SE1OO", "C1SE1H", "C1SE1DD", "C1SE1J", "C1SE1E", "C1SE1B", "C1SE1T", 
-                           "C1SE1QQ", "C1SE1AA", "C1SE1I", "C1SE1BB", "C1SE1GG", "C1SE1M", "C1SE1HH", "C1SE1S", "C1SE1KK", 
-                           "C1PA39", "C1PA37")]
+wave3_s <- da36346.0001[,c("M2ID", "C1PRAGE", "C1SA20A", "C1SA20B", "C1SA20C", "C1SA20D", "C1SA20E", "C1SA20F")]
 
 # Create variables indicating whether people participated in wave: YES
 wave1_s$wave1 <- 1
